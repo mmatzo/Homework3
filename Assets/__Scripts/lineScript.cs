57a2f6403cf4f9bc0ca72449b440ebde328c474a
David <greatcobra@comcast.net>
2020-10-19 13:14:20 -0400

    first iteration of linescript
    complete failure

---
+++ Assets/__Scripts/lineScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineScript : MonoBehaviour
+{
+    public GameObject sphere;
+    public GameObject launchPoint;
+    LineRenderer lineRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRend = gameObject.GetComponent<LineRenderer>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        List<Vector3> pos = new List<Vector3>();
+        pos.Add(sphere.transform.position);
+        pos.Add(launchPoint.transform.position);
+        lineRend.startWidth = 10f;
+        lineRend.endWidth = 10f;
+        lineRend.SetPositions(pos.ToArray());
+        lineRend.useWorldSpace = true;
+    }
+}
